c19d0235f784b71262994b87df0430cedb38ab79
zry <admin@z-touhou.org>
2016-01-26 05:04:21 +0800

    Signed-off-by: zry <admin@z-touhou.org>

--- forms.py
+++ forms.py
@@ -44,14 +44,36 @@ class EditPostForm(forms.ModelForm):
 class PostPermForm(forms.ModelForm):
 	class Meta:
 		model = BlogPost
-		#fields = ['private','passwdlck','passwd','readgrp','readuin','readuex','freecomment','commentgrp','commentuin','commentuex']
-		fields = ['private','passwdlck','passwd','freecomment']
+		fields = ['private','passwdlck','passwd','readgrp','readuin','readuex','freecomment','commentgrp','commentuin','commentuex']
+		#fields = ['private','passwdlck','passwd','freecomment']
 		widgets = {
-			"private":forms.CheckboxInput(attrs={'class':'form-control switch'}),
+			"private":forms.CheckboxInput(attrs={'class':'form-control'}),
 			"passwdlck":forms.CheckboxInput(attrs={'class':'form-control'}),
+			"passwd":forms.TextInput(attrs={'class':'form-control',"placeholder":"请输入访问密码"}),
+			"readgrp":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
+			"readuin":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
+			"readuex":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
+			"freecomment":forms.CheckboxInput(attrs={'class':'form-control'}),
+			"commentgrp":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
+			"commentuin":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
+			"commentuex":form.SelectMultiple(attrs={'class':'form-control','size':'10','multiple':'multiple'},choices=()),
 		}
 
 	def __init__(self,*args,**kwargs):
 		super(PostPermForm,self).__init__(*args,**kwargs)
 		self.fields['private'].label=u'设为私密文章'
 		self.fields['passwdlck'].label=u'使用密码保护'
+		self.fields['passwd'].label=u'访问密码'
+		self.fields['readgrp'].label=u'允许访问的用户组'
+		self.fields['readgrp'].required=False
+		self.fields['readuin'].label=u'额外允许访问的用户'
+		self.fields['readuin'].required=False
+		self.fields['readuex'].label=u'额外不允许访问的用户'
+		self.fields['readuex'].required=False
+		self.fields['freecomment'].label=u'允许任何人评论'
+		self.fields['commentgrp'].label=u'允许评论的用户组'
+		self.fields['commentgrp'].required=False
+		self.fields['commentuin'].label=u'额外允许评论的用户'
+		self.fields['commentuin'].required=False
+		self.fields['commentuex'].label=u'额外不允许评论的用户'
+		self.fields['commentuex'].required=False
